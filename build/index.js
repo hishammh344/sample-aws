b7790b5dc459e1b42f53b1fd97a50782ffcec688
Hisham Mehmood Khan <hishammehmood10@gmail.com>
2023-09-18 15:21:54 +0300

    ds

--- build/index.js
+++ build/index.js
@@ -8,7 +8,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get("/", (req, res) => {
-  res.send("hello!! tis is my first applications runs on ec2");
+  res.json("hello!! tis is my first applications runs on ec2");
 });
 app.listen(8080, () => {
   console.log("listening on 8080");
