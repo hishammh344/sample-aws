305ceba90704168252d9beda1de5634395e8f518
Hisham Mehmood Khan <hishammehmood10@gmail.com>
2023-09-18 13:48:31 +0300

    change port

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("hello!! tis is my first applications runs on ec2");
 });
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
+app.listen(80, () => {
+  console.log("listening on 80");
 });
