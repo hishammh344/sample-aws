69cdd476403fb5cb2a7b7adb46e852c3a4cbf971
Hisham Mehmood Khan <hishammehmood10@gmail.com>
2023-09-18 14:15:13 +0300

    roolback to 3000

--- build/index.js
+++ build/index.js
@@ -8,6 +8,6 @@ const app = (0, express_1.default)();
 app.get("/", (req, res) => {
     res.send("hello!! tis is my first applications runs on ec2");
 });
-app.listen(80, () => {
-    console.log("listening on 80");
+app.listen(3000, () => {
+    console.log("listening on 3000");
 });
