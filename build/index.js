c44d1086ddfcc9d5178282c2fa1f0b60a2b8d5d6
Hisham Mehmood Khan <hishammehmood10@gmail.com>
2023-09-18 14:25:45 +0300

    port to 8080

--- build/index.js
+++ build/index.js
@@ -1,13 +1,15 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get("/", (req, res) => {
-    res.send("hello!! tis is my first applications runs on ec2");
+  res.send("hello!! tis is my first applications runs on ec2");
 });
-app.listen(3000, () => {
-    console.log("listening on 3000");
+app.listen(8080, () => {
+  console.log("listening on 8080");
 });
